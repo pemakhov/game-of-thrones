b0154df6727267f1b4d5228e23e9d30a5e2ff2d5
pemakhov <serhiy.pemakhov@gmail.com>
2019-09-29 23:43:03 +0300

    ps2 added validation and transition to the second page

---
+++ js/script.js
@@ -0,0 +1,37 @@
+const emailPattern = /^[A-Za-z\d][\w.-]+[A-Za-z\d]@[A-Za-z\d][\w-]+[A-Za-z\d]\.[A-Za-z]{2,3}$/;
+const passPattern = /^[\w!@#$%^&*()_+-=]{8,32}$/;
+const namePattern = /^[A-Za-z][a-z0-9]{2,15}$/;
+
+const validateEmail = (email) => emailPattern.test(email);
+const validatePass = (pass) => passPattern.test(pass);
+const goForeward = () => {
+  document.getElementById('first-page').style.display = 'none';
+  document.getElementById('second-page').style.display = 'block';
+};
+
+const validateSignUp = () => {
+  const isValidEmail = validateEmail(document.getElementById('email').value);
+  const isValidPass = validatePass(document.getElementById('password').value);
+  if (isValidEmail && isValidPass) {
+    goForeward();
+  } else {
+    const emailInput = document.getElementById('email');
+    const passInput = document.getElementById('password');
+    if (!isValidEmail) {
+      emailInput.classList.add('invalid-input');
+      passInput.classList.add('invalid-input');
+    }
+    emailInput.addEventListener('focusin', () => {
+      emailInput.classList.remove('invalid-input');
+    });
+    passInput.addEventListener('focusin', () => {
+      passInput.classList.remove('invalid-input');
+    });
+    emailInput.addEventListener('focusout', () => {
+      if (!validateEmail(document.getElementById('email').value)) emailInput.classList.add('invalid-input');
+    });
+    passInput.addEventListener('focusout', () => {
+      if (!validatePass(document.getElementById('password').value)) passInput.classList.add('invalid-input');
+    });
+  }
+};
