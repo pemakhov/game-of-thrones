d448402db0f3d0489915a8afe73c06b30c3a9783
pemakhov <serhiy.pemakhov@gmail.com>
2019-10-09 17:30:10 +0300

    started adding validation to the second page

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 /* RegExp test input patterns */
 const emailPattern = /^[A-Za-z\d][\w.-]+[A-Za-z\d]@[A-Za-z\d][\w-]+[A-Za-z\d]\.[A-Za-z]{2,3}$/;
 const passPattern = /^[\w!@#$%^&*()_+-=]{8,32}$/;
+const namePattern = /[A-Za-z\d]\+/;
+const hobbiesPattern = /^[\w!@#$%^&*()_+-=]{8,500}/;
 
 /* Input validators */
 const validateEmail = (email) => emailPattern.test(email);
 const validatePass = (pass) => passPattern.test(pass);
+const validateName = (name) => namePattern.test(name);
+const validateHobbies = (hobbies) => hobbiesPattern.test(hobbies);
 
 /* Conceils the code related to the first page and reveals the second page */
 const goForeward = () => {
@@ -41,6 +45,36 @@ const validateSignUp = () => {
     });
     passInput.addEventListener('focusout', () => {
       if (!validatePass(document.getElementById('password').value)) passInput.classList.add('invalid-input');
+    });
+  }
+};
+
+/* Highlights the fields that are filled in wrongly */
+const validateSave = () => {
+  const validName = validateName(document.getElementById('name').value);
+  const hobbies = validateHobbies(document.getElementById('hobbies').value);
+  if (validName && validHobbies) {
+    goForeward();
+  } else {
+    const nameInput = document.getElementById('name');
+    const hobbiesArea = document.getElementById('hobbies');
+    if (!validName) {
+      nameInput.classList.add('invalid-input');
+    }
+    if (!validPass) {
+      hobbiesArea.classList.add('invalid-input');
+    }
+    nameInput.addEventListener('focusin', () => {
+      nameInput.classList.remove('invalid-input');
+    });
+    hobbiesArea.addEventListener('focusin', () => {
+      hobbiesArea.classList.remove('invalid-input');
+    });
+    nameInput.addEventListener('focusout', () => {
+      if (!validateName(document.getElementById('name').value)) nameInput.classList.add('invalid-input');
+    });
+    hobbiesArea.addEventListener('focusout', () => {
+      if (!validateHobbies(document.getElementById('hobbies').value)) hobbiesArea.classList.add('invalid-input');
     });
   }
 };
