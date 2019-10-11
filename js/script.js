e15b5847fa4cb57a4a3d7c67e8d91360bfeebdeb
pemakhov <serhiy.pemakhov@gmail.com>
2019-10-11 21:10:54 +0300

    Completely reworked the script. Now it validates two pages. And just works better.

--- js/script.js
+++ js/script.js
@@ -5,88 +5,135 @@ const namePattern = /([A-Za-z\d]+)$/;
 const hobbiesPattern = /[\w!@#$%^&*()_+-=]{8,500}$/;
 
 /* Input validators */
-const validateEmail = (email) => emailPattern.test(email);
-const validatePass = (pass) => passPattern.test(pass);
-const validateName = (name) => namePattern.test(name);
-const validateHobbies = (hobbies) => hobbiesPattern.test(hobbies);
+const emailIsValid = () => emailPattern.test(document.getElementById('email').value);
+const passIsValid = () => passPattern.test(document.getElementById('password').value);
+const nameIsValid = () => namePattern.test(document.getElementById('name').value);
+const hobbiesAreValid = () => hobbiesPattern.test(document.getElementById('hobbies').value);
+const houseIsSelected = () => document.getElementById('house').value !== '0';
 
-/* Conceils the code related to the first page and reveals the second page */
+/* Checks when document is ready and runs a function */
+const documetIsReady = (fn) => {
+  // see if DOM is already available
+  if (document.readyState === 'complete' || document.readyState === 'interactive') {
+    // call on next available tick
+    setTimeout(fn, 1);
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+};
+
+/* Validates input while typing.
+ * input - an input to validate;
+ * validator - the function that validates the input.
+ */
+const validateOnType = (input, validator) => {
+  input.addEventListener('keyup', () => {
+    if (validator()) {
+      input.classList.remove('invalid-input');
+    } else {
+      input.classList.add('invalid-input');
+    }
+  });
+};
+
+/* Adds or removes class indicating invalid input
+ * to the select/option input
+ */
+const validateHouseIsSelected = () => {
+  const houseSelect = document.getElementById('house');
+  if (!houseIsSelected()) {
+    houseSelect.classList.add('invalid-input');
+  } else {
+    houseSelect.classList.remove('invalid-input');
+  }
+};
+
+/* Validates the first page */
+const validateSignUp = (emailInput, passInput) => {
+  emailInput.addEventListener('focusout', () => {
+    if (!emailIsValid()) {
+      emailInput.classList.add('invalid-input');
+      validateOnType(emailInput, emailIsValid);
+    }
+  });
+  passInput.addEventListener('focusout', () => {
+    if (!passIsValid()) {
+      passInput.classList.add('invalid-input');
+      validateOnType(passInput, passIsValid);
+    }
+  });
+};
+
+/* Validates the second page */
+const validateDetailedInfo = (nameInput, houseSelect, hobbiesArea) => {
+  nameInput.addEventListener('focusout', () => {
+    if (!nameIsValid()) {
+      nameInput.classList.add('invalid-input');
+      validateOnType(nameInput, nameIsValid);
+    }
+  });
+  hobbiesArea.addEventListener('focusout', () => {
+    if (!hobbiesAreValid()) {
+      hobbiesArea.classList.add('invalid-input');
+      validateOnType(hobbiesArea, hobbiesAreValid);
+    }
+  });
+  houseSelect.addEventListener('change', validateHouseIsSelected);
+};
+
+/* Conceils the code related to the first page and reveals the second page,
+ * if all inputs passed validation.
+ */
 const goForeward = () => {
   document.getElementById('first-page').style.display = 'none';
   document.getElementById('second-page').style.display = 'block';
+  const nameInput = document.getElementById('name');
+  const houseSelect = document.getElementById('house');
+  const hobbiesArea = document.getElementById('hobbies');
+  const saveButton = document.getElementById('save');
+  validateDetailedInfo(nameInput, houseSelect, hobbiesArea);
+  saveButton.addEventListener('click', () => {
+    if (nameIsValid() && hobbiesAreValid() && houseIsSelected()) {
+      alert('Thank you for signing up and filling in information.'
+        + ' But our websight is not finished yet.'
+        + ' We are terribly sorry.');
+    } else {
+      if (!nameIsValid()) {
+        nameInput.classList.add('invalid-input');
+        validateOnType(nameInput, nameIsValid);
+      }
+      if (!hobbiesAreValid()) {
+        hobbiesArea.classList.add('invalid-input');
+        validateOnType(hobbiesArea, hobbiesAreValid);
+      }
+      if (!houseIsSelected()) {
+        houseSelect.classList.add('invalid-input');
+        houseSelect.addEventListener('change', validateHouseIsSelected);
+      }
+    }
+  });
 };
 
-/* Highlights the fields that are filled in wrongly
- * When all fields are filled in correctly, goes to
- * the second page.
+/* Shows the message about successful validation,
+ * if all inputs passed validation.
  */
-const validateSignUp = () => {
-  const isValidEmail = validateEmail(document.getElementById('email').value);
-  const isValidPass = validatePass(document.getElementById('password').value);
-  if (isValidEmail && isValidPass) {
-    goForeward();
-  } else {
-    const emailInput = document.getElementById('email');
-    const passInput = document.getElementById('password');
-    if (!isValidEmail) {
-      emailInput.classList.add('invalid-input');
+documetIsReady(() => {
+  const emailInput = document.getElementById('email');
+  const passInput = document.getElementById('password');
+  const signUpButton = document.getElementById('sign-up');
+  validateSignUp(emailInput, passInput);
+  signUpButton.addEventListener('click', () => {
+    if (emailIsValid() && passIsValid()) {
+      goForeward();
+    } else {
+      if (!emailIsValid()) {
+        emailInput.classList.add('invalid-input');
+        validateOnType(emailInput, emailIsValid);
+      }
+      if (!passIsValid()) {
+        passInput.classList.add('invalid-input');
+        validateOnType(passInput, passIsValid);
+      }
     }
-    if (!isValidPass) {
-      passInput.classList.add('invalid-input');
-    }
-    emailInput.addEventListener('focusin', () => {
-      emailInput.classList.remove('invalid-input');
-    });
-    passInput.addEventListener('focusin', () => {
-      passInput.classList.remove('invalid-input');
-    });
-    emailInput.addEventListener('focusout', () => {
-      if (!validateEmail(document.getElementById('email').value)) emailInput.classList.add('invalid-input');
-    });
-    passInput.addEventListener('focusout', () => {
-      if (!validatePass(document.getElementById('password').value)) passInput.classList.add('invalid-input');
-    });
-  }
-};
-
-/* Highlights the fields that are filled in wrongly */
-const validateSave = () => {
-  const validName = validateName(document.getElementById('name').value);
-  const validHobbies = validateHobbies(document.getElementById('hobbies').value);
-  const validHouse = document.getElementById('house').value !== 0;
-  console.log(validHouse);
-  if (validName && validHobbies && validHouse) {
-    alert('Cool!');
-  } else {
-    const nameInput = document.getElementById('name');
-    const hobbiesArea = document.getElementById('hobbies');
-    const houseSelect = document.getElementById('house');
-    if (!validName) {
-      nameInput.classList.add('invalid-input');
-    }
-    if (!validHobbies) {
-      hobbiesArea.classList.add('invalid-input');
-    }
-    if (!validHouse) {
-      houseSelect.classList.add('invalid-input');
-    }
-    nameInput.addEventListener('focusin', () => {
-      nameInput.classList.remove('invalid-input');
-    });
-    hobbiesArea.addEventListener('focusin', () => {
-      hobbiesArea.classList.remove('invalid-input');
-    });
-    houseSelect.addEventListener('focusin', () => {
-      houseSelect.classList.remove('invalid-input');
-    });
-    nameInput.addEventListener('focusout', () => {
-      if (!validateName(document.getElementById('name').value)) nameInput.classList.add('invalid-input');
-    });
-    hobbiesArea.addEventListener('focusout', () => {
-      if (!validateHobbies(document.getElementById('hobbies').value)) hobbiesArea.classList.add('invalid-input');
-    });
-    houseSelect.addEventListener('focusout', () => {
-      if (document.getElementById('house').value !== 0) houseSelect.classList.add('invalid-input');
-    });
-  }
-};
+  });
+});
